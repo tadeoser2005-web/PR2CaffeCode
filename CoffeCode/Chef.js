cec40cfd0b1671b5dbf39dc0b95f65d20aec6d37
rojouribej-afk <rojouribej@gmail.com>
2026-09-23 09:53:00 -0600

    Añadí funciones de buscar productos

--- CoffeCode/Chef.js
+++ CoffeCode/Chef.js
@@ -9,8 +9,8 @@ const Cocina = {
 
  
   
-agregar: function(id, nombre, precio) {
-    const nuevoProducto = {id: id, nombre: nombre, precio: precio}; //aqui indicamos que es un objeto y le asignamos los valores de los parametros 
+agregar: function(id, nombre, precio, categoria) { //agrega productos al inventario
+    const nuevoProducto = {id: id, nombre: nombre, precio: precio, categoria: categoria}; //aqui indicamos que es un objeto y le asignamos los valores de los parametros 
     inventarioProductos.push(nuevoProducto); //agregamos el objeto al arreglo
 },
 
@@ -40,17 +40,60 @@ module.exports = Cocina;
 
 
 
-Promociones = {id: 1, nombre: "Descuento 10%", descuento: 0.1,
-              id: 2, nombre: "Descuento 20%", descuento: 0.2,
-              id: 3, nombre: "Descuento 30%", descuento: 0.3};
+const Promociones = {id: 1, nombre: "Descuento 10% en Cafes", descuento: 0.1,
+              id: 2, nombre: "Descuento 20% en Postres", descuento: 0.2,
+              id: 3, nombre: "Descuento 30% en Hamburgesas", descuento: 0.3};
 
-let AgregarPromociones = {
+const AgregarPromociones = {
     agregarPromocion: function(id, nombre, descuento) {
-        const nuevaPromocion = {id: id, nombre: nombre, descuento: descuento};
+        const Promociones = {id: id, nombre: nombre, descuento: descuento};
         inventarioProductos.push(Promociones);
     }
 };
 
+const BuscarProductoCaro = {
+    buscarCaro: function() {
+        let caro = inventarioProdcutos.filter(p => p.precio > 150);
+        return caro;
+    }
+}
+
+const BuscarProductoBarato = {
+    buscarBarato: function(){
+        let barato = inventarioProductos.filter(p => p.precio < 150);
+        return barato;
+    }
+}
+
+const BuscarBebidas = {
+    buscarBebidas: function(){
+        let bebidas = inventarioProductos.find(p => p.categoria === "bebida");
+        return bebidas;
+    }   
+}
+
+const BuscarPostres = {
+    buscarPostres: function(){
+  let postres = inventarioProductos.find(p => p.categoria === "postres");
+    return postres;
+
+    }
+
+}
+
+/* Solo hay que modificar el main para usar estas funciones, añadir la categoria a los productos y ver si el find funciona o lo cambiamos por filter */
+
+
+
+module.exports = BuscarPostres;
+
+module.exports = BuscarBebidas;
+
+module.exports = Promociones;
+
+module.exports = BuscarProductoCaro;
+
+module.exports = BuscarProductoBarato;
 
 module.exports = AgregarPromociones;
 
