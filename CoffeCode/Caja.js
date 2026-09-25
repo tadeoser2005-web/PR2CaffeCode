7759d0c02d854fc0fba71bc3d00799553d24a25e
jesus-garcia5 <124049284@upq.edu.mx>
2026-09-24 23:37:07 -0600

    commit caja parte2
    Refactor totalVenta to use reduce and destructuring for calculations.

--- CoffeCode/Caja.js
+++ CoffeCode/Caja.js
@@ -3,16 +3,30 @@ let totalAcumulado = 0;
 
 const caja = {
     //agrega el pedido a la lista
-        agregarPedido: function(pedido) {
+    agregarPedido: function(pedido) {
         listaDePedidos.push(pedido);
         totalAcumulado += pedido.total;
     },
 
     totalVenta: function() {
-        console.log(`Total de pedidos capturados: ${listaDePedidos.length}`);
-        console.log(`total acumulado: $${totalAcumulado}`);
+        //se utiliza reduce y dertructuring en listaDePedidos
+        const { subtotal, iva, total } = listaDePedidos.reduce(
+            (acumulador, pedido) => {
 
-       
+                const { total } = pedido;//aqui se utiliza solo destructuring para sacar total de pedidos
+                acumulador.subtotal += total;
+                return acumulador;
+            },
+            { subtotal: 0, iva: 0, total: 0 }
+        );
+
+        const ivaCalculado = subtotal * .16;
+        const totalFinal = subtotal + ivaCalculado;
+
+        console.log(`total de pedidos: ${listaDePedidos.length}`);
+        console.log(`subtotal: $${subtotal.toFixed(2)}`);
+        console.log(`iva: $${ivaCalculado.toFixed(2)}`);
+        console.log(`total: $${totalFinal.toFixed(2)}`);
     }
 };
 
